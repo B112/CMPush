41fc4ee1f9305cdca82e8371668bacae0962d28c
Bert Smalen <bert@FiftyTwoDegreesNorth.com>
2020-11-18 20:28:15 +0100

    link

--- Package.swift
+++ Package.swift
@@ -16,5 +16,5 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets:[
-        .binaryTarget(name: "CMPushPackage", url: "https://github.com/cmdotcom/text-push-library-ios/blob/main/CMPush.xcframework.zip", checksum: "676a4b6d1d51cd10979d1902919f82fa3d4a04b9c2ce5871bdc85d1e2ddfd93e")
+        .binaryTarget(name: "CMPushPackage", url: "https://test.qbot.eu/CMPush.xcframework.zip", checksum: "676a4b6d1d51cd10979d1902919f82fa3d4a04b9c2ce5871bdc85d1e2ddfd93e")
         ])
